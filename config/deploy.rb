12e04664e09fa6f30a72cae25764fa914ccf0e0d
Brian Webb <brian@brian-webbs-macbook-pro-2.local>
2010-06-17 15:35:25 -0400

    adsf

--- config/deploy.rb
+++ config/deploy.rb
@@ -89,6 +89,7 @@ after "deploy:restart_passenger", "delayed_job:restart"
 =end
 
 
+
 after "deploy:start", "dj:start"
 after "deploy:stop", "dj:stop"
 
@@ -100,22 +101,22 @@ namespace :dj do
   
   desc "Start delayed_job daemon."
   task :start, :roles => :app do
-    run "if [ -d #{current_path} ]; then cd #{current_path} && sudo RAILS_ENV=#{rails_env} script/delayed_job start; fi"
+    run "if [ -d #{current_path} ]; then cd #{current_path} && sudo RAILS_ENV=production script/delayed_job start; fi"
   end
   
   desc "Stop delayed_job daemon."
   task :stop, :roles => :app do
-    run "if [ -d #{current_path} ]; then cd #{current_path} && sudo RAILS_ENV=#{rails_env} script/delayed_job stop; fi"
+    run "if [ -d #{current_path} ]; then cd #{current_path} && sudo RAILS_ENV=production script/delayed_job stop; fi"
   end
 
   desc "Restart delayed_job daemon."
   task :restart, :roles => :app do
-    run "if [ -d #{current_path} ]; then cd #{current_path} && sudo RAILS_ENV=#{rails_env} script/delayed_job restart; fi"
+    run "if [ -d #{current_path} ]; then cd #{current_path} && sudo RAILS_ENV=production script/delayed_job restart; fi"
   end
 
   desc "Show delayed_job daemon status."
   task :status, :roles => :app do
-    run "if [ -d #{current_path} ]; then cd #{current_path} && sudo RAILS_ENV=#{rails_env} script/delayed_job status; fi"
+    run "if [ -d #{current_path} ]; then cd #{current_path} && sudo RAILS_ENV=production script/delayed_job status; fi"
   end
 
   desc "List the PIDs of all running delayed_job daemons."
@@ -126,7 +127,7 @@ namespace :dj do
   desc "Kill all running delayed_job daemons."
   task :kill, :roles => :app do
     run "sudo lsof | grep '#{deploy_to}/shared/log/delayed_job.log' | cut -c 1-21 | uniq | awk '/^ruby/ {if(NR > 0){system(\"kill -9 \" $2)}}'"
-    run "if [-d #{current_path} ]; then cd #{current_path} && sudo RAILS_ENV=#{rails_env} script/delayed_job stop; fi" # removes orphaned pid file(s)
+    run "if [-d #{current_path} ]; then cd #{current_path} && sudo RAILS_ENV=production script/delayed_job stop; fi" # removes orphaned pid file(s)
   end
   
 end
