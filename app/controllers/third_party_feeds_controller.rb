008830248805b724358ea438b44d01a36aa14c71
Brian Webb <brian@brian-webbs-macbook-pro-2.local>
2010-06-20 20:22:24 -0400

    disabled feed

--- app/controllers/third_party_feeds_controller.rb
+++ app/controllers/third_party_feeds_controller.rb
@@ -6,6 +6,8 @@ class ThirdPartyFeedsController < ApplicationController
       
       # webgains CSV id=1
       if feed_id == 1
+        
+        render :text => "This feed has been temporarily disabled.  Please call 502-254-4324.", :status => 500
       
         @products = Product.find(:all, :conditions => {:visible => true})
      
