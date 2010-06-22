8a94657714116273719c5a2da45e6df2ade31dce
Brian Webb <brian@brian-webbs-macbook-pro-2.local>
2010-06-22 13:44:26 -0400

    fix

--- app/models/third_party_category.rb
+++ app/models/third_party_category.rb
@@ -7,7 +7,7 @@ class ThirdPartyCategory < ActiveRecord::Base
   
   
   def self.grouped_options(third_party)
-    options = ["!! BLANK !!", ["!! BLANK !!", ""]]
+    options = ["!! BLANK !!", [["!! BLANK !!", ""]]]
     
     ThirdPartyCategory.find_all_by_owner(third_party).group_by(&:parent).each do |parent, categories|
       
