14e1be30195a364854440b17c7d286a3f557d5d6
Brian Webb <brian@parkersmithsoftware.com>
2010-07-21 17:45:34 -0400

    asdf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -50,9 +50,9 @@ class ApplicationController < ActionController::Base
   
   
   def set_cache_buster
-    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+    headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    headers["Pragma"] = "no-cache"
+    headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
   
   
