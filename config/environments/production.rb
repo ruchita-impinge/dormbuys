b9f596f0d288b5a4ce34a31154a90017ce58876a
Brian Webb <brian@parkersmithsoftware.com>
2010-08-18 12:15:11 -0400

    asdf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ config.action_view.cache_template_loading            = true
 # Enable threaded mode
 # config.threadsafe!
 
-ENV["RAILS_ASSET_ID"] = '201008181144'
+ENV["RAILS_ASSET_ID"] = '201008181215'
 
 
 # CACHE KEYS IN PLAY
