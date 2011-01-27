845ebb01138b3f9c897f10f36bc9e6d20dd19183
Brian Webb <brian@parkersmithsoftware.com>
2011-01-27 06:18:56 -0500

    adsf

--- public/javascripts/standalone/wrap_up_america/team_picker.js
+++ public/javascripts/standalone/wrap_up_america/team_picker.js
@@ -2,7 +2,7 @@ var none = new Array("No Teams Available");
 var louisville = new Array("Alpha Omicron Pi","Chi Omega","Delta Zeta","Kappa Delta","Pi Beta Phi","Sigma Kappa","ROTC","UofL Athletics");
 var toledo = new Array("General Community Donations", "Alpha Sigma Phi", "Kappa Delta Rho", "Phi Gamma Delta", "Phi Kappa Psi", "Pi Kappa Alpha", "Pi Kappa Phi", "Sigma Alpha Epsilon", "Sigma Phi Epsilon", "Theta Chi", "Theta Tau", "Triangle", "Alpha Xi Delta", "Alpha Omicron Pi", "Alpha Chi Omega", "Delta Delta Delta", "Chi Omega", "Kappa Delta", "Pi Beta Phi", "CAP", "Alpha Phi Omega", "College Republicans", "Gesu Underground");
 var pitt = new Array("Sigma Chi","Kappa Delta","Delta Zeta","Chi Omega","Tri Delta","Tri Sigma","Kappa Kappa Gamma","Alpha Delta Pi");
-var michigan = new Array("Alpha Chi Omega", "Alpha Epsilon Phi", "Zeta Tau Alpha", "Phi Rho Alpha", "Sigma Chi Fraternity");
+var michigan = new Array("Alpha Chi Omega", "Alpha Epsilon Phi", "Zeta Tau Alpha", "Phi Rho Alpha", "Sigma Chi Fraternity", "Delta Delta Delta", "Delta Gamma", "Circle K");
 
 $(document).ready(function() {
 	$('select.campus_selector').eq(0).change(function() {
